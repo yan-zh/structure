211e713bf5092191decf9f868c62e6f971ffea9c
yan-zh <55623887+yan-zh@users.noreply.github.com>
2021-05-30 08:49:27 +0800

    666

--- core/src/com/mygdx/game/Level2/Stage2.java
+++ core/src/com/mygdx/game/Level2/Stage2.java
@@ -39,7 +39,7 @@ public class Stage2 extends MyStage {
     public Stage2(InputMultiplexer inputMultiplexer){
         //注意如果把某些类的实体加入到ActConstants中，需要看一下使用的顺序，有时候先使用了ActConstants中的对象，但是这个对象是在后面才被加入的，可能会空指针异常
 
-
+//666
 //12312312
         inputMultiplexer.addProcessor(this);//加入监听,接受来自最外层的信息。最外层的用户动作信息通过这个分配到各个stage
 
