f7325da392ca5e363c32adea0067c6a5a371fefa
yan-zh <55623887+yan-zh@users.noreply.github.com>
2021-06-07 12:08:09 +0800

    待合并+yzh1

--- core/src/com/mygdx/game/Level2/Stage2.java
+++ core/src/com/mygdx/game/Level2/Stage2.java
@@ -51,7 +51,7 @@ public class Stage2 extends MyStage {
 
         //stage2的第一个演员，如果这个演员的某些函数需要在其他类的实体中被调用，可以选择把它的引用放在ActConstants里
         //添加常规演员，是关卡一开始就有的演员。子弹之类的临时的或在某些特定条件下出现的演员在监听函数里添加
-        this.addActor(new MainCharacter(world,0,40f));//单位是米
+        this.addActor(new MainCharacter(world,5,90f));//单位是米
 
         //每个舞台自己准备摄像机
         boxRender = new Box2DDebugRenderer();//物理实体绘制器，用于绘制物理实体形状
